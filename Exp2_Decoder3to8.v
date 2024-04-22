db6885ab36773e55c4d219635c68af69251bd929
naveen0814 <161302822+naveen0814@users.noreply.github.com>
2024-04-22 13:44:41 +0530

    Update Exp2_Decoder3to8.v

--- Exp2_Decoder3to8.v
+++ Exp2_Decoder3to8.v
@@ -1,12 +1,12 @@
-module decoder_3to8(
-    input [2:0] a,
-    output [7:0] d );
-assign d[0]=(~a[2])&(~a[1])&(~a[0]);
-assign d[1]=(~a[2])&(~a[1])&(a[0]);
-assign d[2]=(~a[2])&(a[1])&(~a[0]);
-assign d[3]=(~a[2])&(a[1])&(a[0]);
-assign d[4]=(a[2])&(~a[1])&(~a[0]);
-assign d[5]=(a[2])&(~a[1])&(a[0]);
-assign d[6]=(a[2])&(a[1])&(~a[0]);
-assign d[7]=(a[2])&(a[1])&(a[0]);
+module decoder1(a,y);
+input [2:0]a;
+output[7:0]y;
+and(y[0],~a[2],~a[1],~a[0]);
+and(y[1],~a[2],~a[1],a[0]);
+and(y[2],~a[2],a[1],~a[0]);
+and(y[3],~a[2],a[1],a[0]);
+and(y[4],a[2],~a[1],~a[0]);
+and(y[5],a[2],~a[1],a[0]);
+and(y[6],a[2],a[1],~a[0]);
+and(y[7],a[2],a[1],a[0]);
 endmodule
